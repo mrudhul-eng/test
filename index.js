f4e8c7785885eb4a3bc5905ab2276bf3e13b6eb7
mrudhul-eng <53948225+mrudhul-eng@users.noreply.github.com>
2023-01-06 14:12:45 -0500

    src

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-const express = require('express');
-const app = express();
-const axios = require('axios');
-require('dotenv').config();
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-app.get('/api/get-speech-token', async (request, response, next) => {
-    response.setHeader('Content-Type', 'application/json');
-
-    if (process.env.SUBSCRIPTIONKEY !== '' || process.env.REGION !== '') {
-        const headers = { 
-            headers: {
-                'Ocp-Apim-Subscription-Key': process.env.SUBSCRIPTIONKEY,
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        };
-
-        try {
-            const result = await axios.post(`https://${process.env.REGION}.api.cognitive.microsoft.com/sts/v1.0/issueToken`, null, headers);
-            response.send({ token: result.data, region: process.env.REGION });
-        } catch (error) {
-            response.status(401).send('Error authorizing speech key.');
-        }
-    } else {
-        response.status(400).send('Please add your subscription key or region to the .env file.');
-
-    }
-});
-
-app.listen(3001);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
